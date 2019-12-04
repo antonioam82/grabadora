a57ca2840733960404bd00801bb7a10bf610e87b
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-12-04 12:33:29 +0100

    Update Grabadora_GUI.py

--- Grabadora_GUI.py
+++ Grabadora_GUI.py
@@ -2,45 +2,118 @@
 # -*- coding: utf-8 -*-
 from tkinter import Tk,Label,Button,Frame,filedialog
 import pyaudio
+import glob
 import os
 import wave
 import threading
 
 grabando=False
+reproduciendo=False
+CHUNK=1024
+data=""
+stream=""
+f=""
+contador=0
+contador1=0
+contador2=0
+
+def clear_contador():
+    global contador,contador1,contador2
+    contador=0
+    contador1=0
+    contador2=0
 
 def iniciar():
     global grabando
     global proceso
     global act_proceso
-    btnIniciar.config(state='disabled')
-    btnDir.config(state='disabled')
-    #contador=0
+    clear_contador()
+    audio=pyaudio.PyAudio()
+    bloqueo('disabled')
     grabando=True
     FORMAT=pyaudio.paInt16
     CHANNELS=2
     RATE=44100
-    CHUNK=1024
     act_proceso=True
     archivo="grabacion.wav"
-    audio=pyaudio.PyAudio()
     t1=threading.Thread(target=grabacion, args=(FORMAT,CHANNELS,RATE,CHUNK,audio,archivo))
     t=threading.Thread(target=cuenta)
     t1.start()
     t.start()
+
+def formato(c):
+    if c<10:
+        c="0"+str(c)
+    return c
     
-def cuenta(contador=0):
+def cuenta():
     global proceso
-    time['text'] = contador
-    contador+1
-    proceso=time.after(1000, cuenta, (contador+1))
+    global contador,contador1,contador2
+    time['text'] = str(formato(contador1))+":"+str(formato(contador2))+":"+str(formato(contador))
+    contador+=1
+    if contador==60:
+        contador=0
+        contador2+=1
+    if contador2==60:
+        contador2=0
+        contador1+=1
+    proceso=time.after(1000, cuenta)
+
+def abrir():
+    global data
+    global stream
+    global f
+    global reproduciendo
+    clear_contador()
+    audio=pyaudio.PyAudio()
+    open_archive=filedialog.askopenfilename(initialdir = "/",
+                 title = "Seleccione archivo",filetypes = (("wav files","*.wav"),
+                 ("all files","*.*")))
+    if open_archive!="":
+        reproduciendo=True
+        f = wave.open(open_archive,"rb")
+        stream = audio.open(format = audio.get_format_from_width(f.getsampwidth()),  
+                    channels = f.getnchannels(),  
+                    rate = f.getframerate(),
+                    output = True)
+        data = f.readframes(CHUNK)
+        bloqueo('disabled')
+        t=threading.Thread(target=cuenta)
+        t.start()
+        t2=threading.Thread(target=reproduce)
+        t2.start()
+
+def reproduce():
+    global data
+    global stream
+    global f
+    audio=pyaudio.PyAudio() 
+    while data and reproduciendo==True:  
+        stream.write(data)  
+        data = f.readframes(CHUNK)  
+ 
+    stream.stop_stream()  
+    stream.close()  
+ 
+    audio.terminate()
+    time.after_cancel(proceso)
+    bloqueo('normal')
+
+def bloqueo(s):
+    btnIniciar.config(state=s)
+    btnDir.config(state=s)
+    btnAbrir.config(state=s)
     
 def parar():
     global grabando
+    global reproduciendo
     if grabando==True:
         grabando=False
         time.after_cancel(proceso)
-        btnIniciar.config(state='normal')
-        btnDir.config(state='normal')
+        clear_contador()
+    elif reproduciendo==True:
+        reproduciendo=False
+    bloqueo('normal')
 
 def direc():
     directorio=filedialog.askdirectory()
@@ -55,11 +128,9 @@ def grabacion(FORMAT,CHANNELS,RATE,CHUNK,audio,archivo):
 
     frames=[]
 
-    print("GRABANDO")
     while grabando==True:
         data=stream.read(CHUNK)
         frames.append(data)
-    print("fin")
 
     #DETENEMOS GRABACIÓN
     stream.stop_stream()
@@ -67,12 +138,12 @@ def grabacion(FORMAT,CHANNELS,RATE,CHUNK,audio,archivo):
     audio.terminate()
 
     #CREAMOS/GUARDAMOS EL ARCHIVO DE AUDIO
-    count = 0
-    for i in os.listdir():
+    count=0
+    for i in glob.glob("*.wav"):
         if "grabacion" in i:
             count+=1
     if count>0:
-        archivo = "grabacion"+"("+str(count)+")"+".wav"
+        archivo="grabacion"+"("+str(count)+")"+".wav"
         
     waveFile = wave.open(archivo, 'wb')
     waveFile.setnchannels(CHANNELS)
@@ -82,19 +153,21 @@ def grabacion(FORMAT,CHANNELS,RATE,CHUNK,audio,archivo):
     waveFile.close()
 
 ventana = Tk()
-ventana.title('Grabadora')
+ventana.title('Grabadora Audio')
 
-time = Label(ventana, fg='green', width=20, text="0", bg="black", font=("","30"))
+time = Label(ventana, fg='green', width=20, text="00:00:00", bg="black", font=("","30"))
 time.pack()
-ventana.geometry("470x77")
+ventana.geometry("488x77")
  
 frame=Frame(ventana)
-btnIniciar=Button(frame, fg='blue',width=21, text='Iniciar', command=iniciar)
+btnIniciar=Button(frame, fg='blue',width=16, text='Iniciar', command=iniciar)
 btnIniciar.grid(row=1, column=1)
-btnParar=Button(frame, fg='blue', width=21, text='Parar', command=parar)
+btnParar=Button(frame, fg='blue', width=16, text='Parar', command=parar)
 btnParar.grid(row=1, column=2)
-btnDir=Button(frame, text="Directorio",width=21,command=direc)
+btnDir=Button(frame, text="Carpeta",width=16,command=direc)
 btnDir.grid(row=1,column=0)
+btnAbrir=Button(frame, text="Abrir",width=16,command=abrir)
+btnAbrir.grid(row=1,column=3)
 frame.pack()
  
 ventana.mainloop()
